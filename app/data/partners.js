9d4ed04b751e1b8bbf11cd637edf40e4fe79b54f
Espen Fossen <espenaf@junta.no>
2019-01-11 11:16:04 +0100

    Added matiq as partner.

--- app/data/partners.js
+++ app/data/partners.js
@@ -1,6 +1,7 @@
 export default [
     {name: 'Kantega', logo: 'kantega.svg', url: 'https://www.kantega.no/'},
     {name: 'ITsjefen', logo: 'itsjefen.svg', url: 'https://www.itsjefen.no/'},
+    {name: 'Matiq', logo: 'matiq.svg', url: 'http://www.matiq.no/'},
     {name: 'Sintef', logo: 'sintef.svg', url: 'https://www.sintef.no/'},
     {name: 'Sopra Steria', logo: 'soprasteria.svg', url: 'https://www.soprasteria.no/'},
 ];
