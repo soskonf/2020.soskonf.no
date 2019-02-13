60a5f2406ad0f1558412c6c6dd856004b4d57767
Espen Fossen <espenaf@junta.no>
2019-02-13 19:32:27 +0100

    Fixed error commit.

--- app/app.js
+++ app/app.js
@@ -1,10 +1,15 @@
 import 'react-app-polyfill/ie11';
+import reactFlexboxGrid from 'react-flexbox-grid';
+import styles from './styles/App.less';
 import 'whatwg-fetch';
 import 'es6-promise/auto';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import {render} from 'react-dom';
+import * as polyfill from './components/polyfill';
+import * as favicons from './favicon';
+import { render } from 'react-dom';
 import {Provider} from 'react-redux';
-import {store} from './store.js';
+import { store } from './store.js';
+import routes from './routes';
 import Root from './root';
 
 const container = document.querySelector('#app');
