0b1d0ecfa1537caafd3922a2820a8eb6bacb7bd2
Espen Fossen <espenaf@junta.no>
2019-03-04 11:41:52 +0100

    Added CSA logo.

--- app/data/partners.js
+++ app/data/partners.js
@@ -1,4 +1,5 @@
 export default [
+    {name: 'CSA', logo: 'csa.svg', url: 'https://cloudsecurityalliance.org/'},
     {name: 'Kantega', logo: 'kantega.svg', url: 'https://www.kantega.no/'},
     {name: 'ITsjefen', logo: 'itsjefen.svg', url: 'https://www.itsjefen.no/'},
     {name: 'Matiq', logo: 'matiq.svg', url: 'http://www.matiq.no/'},
