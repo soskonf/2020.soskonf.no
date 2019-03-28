f1ba2b031f6d5e07144ddabe2fe275fef670c087
Espen Fossen <espenaf@junta.no>
2019-03-28 21:56:35 +0100

    Added Are and Hans to speaker.

--- app/data/speakers.js
+++ app/data/speakers.js
@@ -4,12 +4,26 @@
 //{name: 'Magnhild Kaasin', title: 'Riksrevisjonen', image: '', url: '', bio: ''},
 
 export default [
+    {
+        name: 'Are Hellandsvik',
+        title: 'Forsker, Sintef Digital',
+        image: 'are_hellandsvik.jpg',
+        url: '',
+        bio: 'Siv.ing i Analog Mikroelektronikk fra NTNU med erfaring til sammen 10 års erfaring fra Atmel og Data Response. Har de siste 6 årene jobbet som forsker ved SINTEF Digital.'
+    },
     {name: 'Beau Woods', title: 'Cyber Safety Advocate, I Am the Cavalry', image: 'beau_woods.jpg', url: 'https://twitter.com/beauwoods', bio: 'Sikkerhetsforsker, Cyber Safety Innovation Fellow i tenkesmien Atlantic Council, og grunder av Stratigos Security. Medgründer av I Am the Cavalry, en non-profit utdanningsorganisasjon som jobber med problemer i skjæringspunktet mellom informasjonssikkerhet, samfunnssikkerhet og helse.'},
     {name: 'Bjarte Aksnes', title: 'Seniorrådgiver, Direktoratet for e-helse', image: 'bjarte_aksnes.jpg', url: '', bio: 'Lang fartstid i Helsedirektoratet og nå i det som heter Direktoratet for e-helse. Bjarte har tidligere arbeidet i KITH AS (Kompetansesenter for IT i helse- og sosialsektoren). Han er siv.ing. og har arbeidet mye med informasjonssikkerhet og arkitektur.'},
     {name: 'Cecilie Wian', title: 'IT Konsulent, Knowit', image: 'cecilie_wian.jpg', url: '', bio: 'Med en bakgrunn i både pedagogikk og it, og et hjerte for brukerne. Jobber som konsulent hos Knowit Consulting Bergen, hovedsaklig med QA'},
     {name: 'Erlend Andreas Gjære', title: 'Spesialist på sikkerhet og folk, Secure Practice', image: 'erlend_gjere.jpg', url: 'https://twitter.com/erlangsec', bio: 'Erlend har mastergrad i informatikk fra NTNU, er sertifisert ISO27001 Lead Implementer, og har tidligere erfaring fra flere år som forsker og ansvarlig for sikkerhet blant ansatte i SINTEF, og som sikkerhetsleder i Signicat. Nå er han gründer og daglig leder i Secure Practice AS, som i 2018 vant Outstanding Security Performance Award for beste nye sikkerhetsprodukt med MailRisk.'},
     {name: 'Guttom Sindre', title: 'Professor, NTNU', image: 'guttorm_sindre.jpg', url: '' , bio: ''},
     {name: 'Per Thorsheim', title: 'Sikkerhetssjef og passordekspert', image: 'per_thorsheim.jpg', url: 'https://twitter.com/thorsheim' , bio: 'Lidenskapelig opptatt av folks dårlige passord, som bidro til at han grunnla http://PasswordsCon.org. Jobber til daglig som sikkerhetssjef, men er en dreven foredragsholder som vet å fange oppmerksomheten til de fleste.  Mest kjent for å i 2012 avslørte at Linkedin var blitt hacket, samt i 2015 med datingtjenesten Ashley Madison. Han er sertifisert CISA, CISM, CISSP & ISSAP.'},
+    {
+        name: 'Hans Petter Holen',
+        title: 'Informasjonssikkerhetsansvarlig, Visma',
+        image: 'hans_petter_holen.jpg',
+        url: 'https://twitter.com/hpholen',
+        bio: 'En av grunnleggerne av Oslonett, første tilbyder av Internett for privatpersoner og SMBer i Norge. Har siden hatt lederstillinger i Schibsted Nett, Scandinavia Online og Tascali. Vært aktiv i ICANN og RIPE i over 20 år, i rollen som chair for RIPE de siste 5 årene.'
+    },
     {name: 'Håkon Grimstad', title: 'Administrerende Direktør, Norsk Helsenett', image: 'hakon_grimstad.jpg', url: '' , bio: 'Jobbet over 10 år som Administrerende Direktør i Norsk Helsenett.'},
     {name: 'Martha Eike', title: 'Fagdirektør, Datatilsynet', image: 'martha_eike.jpg', url: 'https://twitter.com/marthaeike', bio: 'Utdannet dataingeniør med ni års erfaring fra QA og programvareutvikling i privat sektor. Hun har opparbeidet seg erfaring med skytjenester, digitalisering av offentlig sektor, opplæringssektoren, innebygd personvern, vurdering av personvernkonsekvenser og Big Data. Martha koordinerer det utadrettede arbeidet med informasjonssikkerhet.'},
     {name: 'Marie Moe', title: 'Forskningssjef, SINTEF Digital', image: 'marie_moe.jpg', url: 'https://twitter.com/mariegmoe', bio: 'Marie Moe har en mastergrad i matematikk/kryptografi, samt en doktorgrad i informasjonssikkerhet. Marie har erfaring som seksjonsleder ved NSM NorCERT, Norges nasjonale senter for håndtering av alvorlige dataangrep. Hun jobber i dag som forskningssjef ved SINTEF Digital, og underviser ved NTNU. På fritiden er Marie engasjert i grasrot-organisasjonen ”I Am The Cavalry”.'},
