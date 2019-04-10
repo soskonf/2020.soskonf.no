17afe5234aab6d36afdb2956cbc456bc5931be7f
Espen Fossen <espenaf@junta.no>
2019-04-10 13:18:34 +0200

    Added Google Tag Manager with anonymize_ip=true.

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -3,6 +3,6 @@ import { sessions } from './sessions';
 import { session } from './session';
 import { routes } from './routes';
 import { menu } from './menu';
-import { filters } from './filters'
+import { filters } from './filters';
 
 export { workshops, sessions, session, routes, menu, filters };
