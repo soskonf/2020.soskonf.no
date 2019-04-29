812b7eccfd7522221c7fd1c6d8376a85adf0457b
Espen Fossen <espenaf@junta.no>
2019-04-29 10:49:29 +0200

    Added Data Equipment as partner.

--- app/data/partners.js
+++ app/data/partners.js
@@ -1,6 +1,7 @@
 export default [
     {name: 'CSA', logo: 'csa.svg', url: 'https://cloudsecurityalliance.org/'},
     {name: 'Dataforeningen', logo: 'dnd.svg', url: 'https://www.dataforeningen.no/'},
+    {name: 'Data Equipment', logo: 'dataequipment.svg', url: 'https://www.dataequipment.no'},
     {name: 'EQS by extend', logo: 'eqs_by_extend.svg', url: 'https://www.extend.no/eqs/'},
     {name: 'Kantega', logo: 'kantega.svg', url: 'https://www.kantega.no/'},
     {name: 'ITsjefen', logo: 'itsjefen.svg', url: 'https://www.itsjefen.no/'},
