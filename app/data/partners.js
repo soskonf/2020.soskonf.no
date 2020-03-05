914a4ff8f2b9ef0558816d40da00291c958302d0
Espen Fossen <espenaf@junta.no>
2020-03-05 09:42:01 +0100

    Added IBM Security and Palo Alto as Partners.

--- app/data/partners.js
+++ app/data/partners.js
@@ -1,6 +1,8 @@
 export default [
     {name: 'Dataforeningen', logo: 'dnd.svg', url: 'https://www.dataforeningen.no/'},
     {name: 'Kantega', logo: 'kantega.svg', url: 'https://www.kantega.no/'},
+    {name: 'IBM Security', logo: 'ibmsecurity.svg', url: 'https://www.ibm.com/security/'},
     {name: 'ITsjefen', logo: 'itsjefen.svg', url: 'https://www.itsjefen.no/'},
+    {name: 'Palo Alto', logo: 'paloalto.svg', url: 'https://www.paloaltonetworks.com/'},
     {name: 'Sintef', logo: 'sintef.svg', url: 'https://www.sintef.no/'},
 ];
