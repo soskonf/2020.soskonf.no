4a77780b73f52a52924eb6409362d9c2c170f805
Espen Fossen <espenaf@junta.no>
2020-03-05 12:40:14 +0100

    Added speakers.

--- app/data/speakers.js
+++ app/data/speakers.js
@@ -1,2 +1,15 @@
-
-export default [];
+export default [
+    {name: 'Erlend Andreas Gjære', title: 'Spesialist på sikkerhet og folk, Secure Practice', image: 'erlend_gjere.jpg', url: 'https://twitter.com/erlangsec', bio: 'Erlend har mastergrad i informatikk fra NTNU, er sertifisert ISO27001 Lead Implementer, og har tidligere erfaring fra flere år som forsker og ansvarlig for sikkerhet blant ansatte i SINTEF, og som sikkerhetsleder i Signicat. Nå er han gründer og daglig leder i Secure Practice AS, som i 2018 vant Outstanding Security Performance Award for beste nye sikkerhetsprodukt med MailRisk.'},
+    {name: 'Erlend Dyrnes', title: 'Informasjonssikkerhetsansvarlig, Sbanken', image: 'erlend_dyrnes.jpg', url: 'https://twitter.com/erlend_d', bio: ''},
+    {name: 'Gøran Tømte', title: 'CISO, Data Equipment', image: 'goran_tomte.jpg', url: 'https://twitter.com/birifoto', bio: 'Gøran Tømte jobber idealistisk og lidenskapelig for et sikrere Norge. Jobber aktivt med NSM, DSB, Justisdepartementet, ISF, Kins, SANS, diverse CERT’er, Den norske dataforening, NorSIS, Kote, FSK, med flere. Primært er fokuset på proaktiv og preventiv sikkerhet, med tiltak som faktisk gjør en forskjell ved at de hindrer hendelser fra å skje. Fokuset er på Zero Trust som en sikkerhetsstrategi, samt NSM Grunnprinsipper for IKT-sikkerhet eller Center for Internet Security sine Critical Security Controls, som kontroll mekanismer. For tjenester i skyen vil Cloud Security Alliance ha egne kontrollverktøy alle bør etterleve. Gøran sitter i styret hos CSA Norway, samt i Forum for nasjonal IKT sikkerhet underlagt Justisdepartementet.'},
+    {name: 'Kent Heide', title: 'Siberia Technologies', image: 'kent_heide.jpg', url: 'https://github.com/kheide', bio: ''},
+    {name: 'Line Dugstad', title: 'Journalist, Dagens Næringsliv', image: 'line_dugstad.jpg', url: 'https://twitter.com/linedugstad', bio: ''},
+    {name: 'Osman Kibar', title: 'Journalist, Dagens Næringsliv', image: 'osman_kibar.jpg', url: '', bio: ''},
+    {name: 'Péter Gombos', title: 'Sikkerhetskonsulent, Nagarro', image: 'peter_gombos.jpg', url: 'https://twitter.com/petergombos', bio: ''},
+    {name: 'Steinar Noem', title: 'Udelt', image: 'steinar_noem.jpg', url: 'https://github.com/steinarnoem', bio: ''},
+    {name: 'Øivind Høiem', title: 'Seniorrådgiver, Unit', image: 'oyvind_hoiem.jpg', url: 'https://twitter.com/oivindhoiem', bio: ''},
+    {name: 'Øyvind Røset', title: 'Informasjonssikkerhetsrådgiver, Helse Midt-Norge', image: 'oyvind_roset.jpg', url: '', bio: ''},
+    {name: 'Åsmund Skomedal', title: 'Forskningsdirektør, Norsk Regnesentral', image: 'asmund_skomedal.jpg', url: '', bio: ''},
+    {name: 'Roar Thon', title: 'Fagdirektør, NSM', image: 'roar_thon.jpg', url: 'https://twitter.com/secdefence', bio: ''},
+    {name: 'Veronica Jarnskjold Buer', title: 'Fagdirektør, Datatilsynet', image: 'veronica_buer.jpg', url: '', bio: ''}
+];
